3eeff023584382b17f948b691cccebe91279b537
nlpeng <nlpeng@outlook.com>
2019-01-09 15:35:42 +0800

    1.1v 高并发编程

--- concurrency/src/main/java/com/ferry/concurrency/example/threadPool/ThreadPoolExample1.java
+++ concurrency/src/main/java/com/ferry/concurrency/example/threadPool/ThreadPoolExample1.java
@@ -14,12 +14,13 @@ public class ThreadPoolExample1 {
 
         for (int i = 0; i < 10; i++) {
             final int index = i;
-            executorService.execute(new Runnable() {
+            /*executorService.execute(new Runnable() {
                 @Override
                 public void run() {
                     log.info("task:{}", index);
                 }
-            });
+            });*/
+            executorService.execute(() ->log.info("task:{}", index));
         }
         executorService.shutdown();
     }
